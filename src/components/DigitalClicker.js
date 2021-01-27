ebb62d11340e6749f9edf8d95232cd9c74179fba
nadavschwartz <nadavschwartz58@gmail.com>
2021-01-27 14:41:54 -0800

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+    handleClick = () => {
+        this.setState((state) => {
+          return {timesClicked: state.timesClicked + 1}
+        })
+    }
+    render() {
+        return (
+            <div style={{ textAlign: 'center'}}>
+                <button style={{ width: 100, height: 100, marginTop: 100 }} onClick={this.handleClick}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,52 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from "react";
+
+export default class YouTubeDebugger extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      user: null,
+      errors: [],
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: "1080p",
+        },
+      },
+    };
+  }
+  handleClick = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12,
+      },
+    });
+    console.log(this.state.settings.bitrate);
+  };
+    
+    handleChangeResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    ...this.state.settings.video,
+                    resolution: '720p'
+                }
+            }
+        });
+    }
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleClick}>
+          {this.state.settings.bitrate}
+        </button>
+        <button className="resolution" onClick={this.handleChangeResolution}>
+          {this.state.settings.video.resolution}
+        </button>
+      </div>
+    );
+  }
+}
